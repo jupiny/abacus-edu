d0fa0fe7180fe130a9cedae4ec5bfe4ed36a6857
jupiny <tmdghks584@gmail.com>
2016-11-17 23:17:30 +0900

    install debug_toolbar
    add settings related to debug_toolbar

--- abacus_edu/abacus_edu/settings/development.py
+++ abacus_edu/abacus_edu/settings/development.py
@@ -1,1 +1,12 @@
 from .partials import *
+
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE_CLASSES += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ('127.0.0.1',)
--- abacus_edu/abacus_edu/urls.py
+++ abacus_edu/abacus_edu/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
+
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
