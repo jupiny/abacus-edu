81eb46f31e6814a42d9e1f7459c0d9acdf38d334
jupiny <tmdghks584@gmail.com>
2016-11-18 13:00:14 +0900

    refactor Video model
    seperate get_youtube_original_url function into get_youtube_original_url
    and get_youtube_original_url_html_tag functions

--- abacus_edu/videos/admin.py
+++ abacus_edu/videos/admin.py
@@ -4,9 +4,9 @@ from .models import Video
 
 
 class VideoAdmin(admin.ModelAdmin):
-    fields = ('title', 'writer', 'youtube_id', 'get_youtube_original_url',)
-    readonly_fields = ('get_youtube_original_url',)
-    list_display = ('title', 'writer', 'get_youtube_original_url',)
+    fields = ('title', 'writer', 'youtube_id', 'get_youtube_original_url_html_tag',)
+    readonly_fields = ('get_youtube_original_url_html_tag',)
+    list_display = ('title', 'writer', 'get_youtube_original_url_html_tag',)
     search_fields = ['title', ]
 
 admin.site.register(Video, VideoAdmin)
--- abacus_edu/videos/models.py
+++ abacus_edu/videos/models.py
@@ -16,16 +16,19 @@ class Video(models.Model):
         verbose_name="유튜브 ID",
     )
 
+    @property
     def get_youtube_original_url(self):
-        youtube_original_url = "https://www.youtube.com/watch?v={youtube_id}".format(
+        return "https://www.youtube.com/watch?v={youtube_id}".format(
             youtube_id=self.youtube_id,
         )
+
+    def get_youtube_original_url_html_tag(self):
         return format_html(
             "<a href='{}' target='_blank'>{}</a>",
-            youtube_original_url,
-            youtube_original_url,
+            self.get_youtube_original_url,
+            self.get_youtube_original_url,
         )
-    get_youtube_original_url.short_description = "유투브 URL"
+    get_youtube_original_url_html_tag.short_description = "유투브 URL"
 
     class Meta:
         verbose_name = "Video"
