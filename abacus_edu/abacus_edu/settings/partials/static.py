3cd25f05e23ecc09c7f3355de34962178ef684c2
jupiny <tmdghks584@gmail.com>
2016-11-20 16:18:27 +0900

    update static settings
    add STATICFILES_DIRS

--- abacus_edu/abacus_edu/settings/partials/static.py
+++ abacus_edu/abacus_edu/settings/partials/static.py
@@ -1,4 +1,13 @@
+import os
+
+from .base import BASE_DIR
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
