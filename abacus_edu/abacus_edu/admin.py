6abfdbace56d02fe5d74d467d405e59954bfe46d
jupiny <tmdghks584@gmail.com>
2016-11-20 15:29:36 +0900

    update admin
    register Category model

--- abacus_edu/abacus_edu/admin.py
+++ abacus_edu/abacus_edu/admin.py
@@ -3,13 +3,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
 from .models import Video
+from .models import Category
 
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'writer', 'get_youtube_original_url_html_tag',)
     search_fields = ['title', ]
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ['title', ]
+
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
 admin.site.register(Video, VideoAdmin)
+admin.site.register(Category, CategoryAdmin)
