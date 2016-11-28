b097ad9e72ab69aa1825034c301e1eeb6741a306
jupiny <tmdghks584@gmail.com>
2016-11-28 23:47:35 +0900

    init MEDIA settings

--- abacus_edu/abacus_edu/settings/partials/static.py
+++ abacus_edu/abacus_edu/settings/partials/static.py
@@ -1,6 +1,7 @@
 import os
 
 from .base import BASE_DIR
+from .base import PROJECT_ROOT_DIR
 
 
 # Static files (CSS, JavaScript, Images)
@@ -11,3 +12,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# Media files
+MEDIA_ROOT = os.path.join(
+    PROJECT_ROOT_DIR,
+    "dist",
+    "media",
+)
+
+MEDIA_URL = '/media/'
--- abacus_edu/abacus_edu/urls.py
+++ abacus_edu/abacus_edu/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include, patterns
 from django.contrib import admin
-
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
