6786214633a1eefd557add9653b57a08bc8174f2
jupiny <tmdghks584@gmail.com>
2016-12-04 16:14:16 +0900

    update Category model
    change verbose_name of title field

--- abacus_edu/abacus_edu/models/category.py
+++ abacus_edu/abacus_edu/models/category.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     )
     title = models.CharField(
         max_length=20,
-        verbose_name="제목",
+        verbose_name="카테고리 제목",
     )
 
     def __str__(self):
