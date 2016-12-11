53d2f6c5b298daebb9f5be6fe1457704b2835f78
jupiny <tmdghks584@gmail.com>
2016-12-12 01:21:46 +0900

    rename Video model property
    get_youtube_original_url => youtube_original_url
    get_youtube_thumbnail_image_url => youtube_thumbnail_image_url
    get_youtube_embed_url => youtube_embed_url

--- abacus_edu/api/v1/serializers/video.py
+++ abacus_edu/api/v1/serializers/video.py
@@ -6,5 +6,5 @@ from abacus_edu.models import Video
 class VideoModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = ('id', 'title', 'writer', 'youtube_id', 'get_youtube_original_url',
-                  'get_youtube_thumbnail_image_url', 'get_youtube_embed_url',)
+        fields = ('id', 'title', 'writer', 'youtube_id', 'youtube_original_url',
+                  'youtube_thumbnail_image_url', 'youtube_embed_url',)
