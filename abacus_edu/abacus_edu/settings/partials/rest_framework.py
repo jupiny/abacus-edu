2bb1d9d73849833c78f0cb3aa1027657a336d0c7
jupiny <tmdghks584@gmail.com>
2016-12-12 23:56:57 +0900

    install rest_framework_jwt
    add rest_framework_jwt settings in REST_FRAMEWORK settings

--- abacus_edu/abacus_edu/settings/partials/rest_framework.py
+++ abacus_edu/abacus_edu/settings/partials/rest_framework.py
@@ -1,4 +1,15 @@
 REST_FRAMEWORK = {
+    # Pagination
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+
+    # Authentication
+    'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.IsAuthenticated',
+        ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
 }
--- abacus_edu/abacus_edu/urls.py
+++ abacus_edu/abacus_edu/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('api.v1.urls', namespace='api')),
+
+    # Obtain JWT Token
+    url(r'^api-token-auth/', obtain_jwt_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
