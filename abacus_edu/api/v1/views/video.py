cd2282d206aba66ac227bcfd07c28632cd15d2e0
jupiny <tmdghks584@gmail.com>
2016-12-30 20:27:48 +0900

    init AutoplayVideoListAPIView, VideoYoutubeIDSerializer

--- abacus_edu/api/v1/views/video.py
+++ abacus_edu/api/v1/views/video.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from abacus_edu.models import Application, Category, Video
-from api.v1.serializers.video import VideoModelSerializer
+from api.v1.serializers.video import VideoModelSerializer, VideoYoutubeIDSerializer
 
 
 class VideoListAPIView(ListAPIView):
@@ -31,3 +31,12 @@ class RecommendedVideoListAPIView(ListAPIView):
     def get_queryset(self):
         application = get_object_or_404(Application, slug=self.kwargs.get('application_slug'))
         return Video.objects.filter(category__application=application, is_recommended=True)
+
+
+class AutoplayVideoListAPIView(ListAPIView):
+    serializer_class = VideoYoutubeIDSerializer
+
+    def get_queryset(self):
+        application = get_object_or_404(Application, slug=self.kwargs.get('application_slug'))
+        category = get_object_or_404(Category, pk=self.kwargs.get('category_id'))
+        return category.video_set.all()
