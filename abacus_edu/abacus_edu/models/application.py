3e99ce45eb6cb03802497ccaf9f3e1481a2a2bfd
jupiny <tmdghks584@gmail.com>
2017-01-15 19:54:12 +0900

    add description field in Application model

--- abacus_edu/abacus_edu/models/application.py
+++ abacus_edu/abacus_edu/models/application.py
@@ -41,6 +41,7 @@ class Application(Timestampable, models.Model):
         blank=True,
         verbose_name="대표 이미지 (유튜브 영상이 링크가 안 될때 보여주는 이미지)",
     )
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.app_name
